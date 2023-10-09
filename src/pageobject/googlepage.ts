2e0a59aaa0927436ffef5f04f78427cbbb006b38
vinod <vinod236@gmail.com>
2023-10-09 13:40:41 +0530

    added mocking code

--- src/pageobject/googlepage.ts
+++ src/pageobject/googlepage.ts
@@ -1,20 +1,30 @@
 import { basePage } from './basepage';
-import {googlepage_locator as locator} from '../pageobject/locators/googlepage_locator';
+import { locators, getAttr } from './locators/allElemenets';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Page, expect } from '@playwright/test';
 class googlePage extends basePage {
   constructor(public page: Page) {
     super(page);
   }
-  async Search(text: string) {
-    await this.page.locator(locator.searchbox).fill(text);
+  async Search(text: string, elementName: keyof typeof locators) {
+    let json;
+    await this.page!.route(`*/**/complete/search?q=*`, async (route) => {
+      json = [[['vinod\u003cb\u003e cookware\u003c/b\u003e', 0, [71, 598, 432]]]];
+      await route.fulfill({ json });
+    });
+    await this.page.locator(locators[elementName] as string).fill(text);
+    const xpath = `//span[contains(text(),'vinod')]`;
+    await expect(this.page!.waitForSelector(xpath, { timeout: 5000 })).resolves.not.toThrow();
     await this.page.keyboard.press('Enter');
   }
 
-  async VerifySearchResult(text: string) {
-    await this.page.waitForSelector(locator.searchvalue(text));
-    const result = await this.page.locator(locator.searchvalue(text)).innerText();
-    await expect(result).toContain(text);
+  async VerifySearchResult(text: string, elementName: keyof typeof locators) {
+    // eslint-disable-next-line no-console
+    console.log('locators[elementName]', elementName);
+    const locatorString = getAttr(elementName, text);
+    await this.page.waitForSelector(locatorString);
+    const result = await this.page.locator(locatorString).innerText();
+    return result;
   }
 }
 export const googlepage = (page: Page) => new googlePage(page);
